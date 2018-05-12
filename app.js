4c8c4bd9f26ad46bd9a4cee6cc89e117002da132
Your Name <you@example.com>
2018-05-12 15:11:59 +0400

    Getting started

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+//Server
+app.use(express.static("./public"));
+
+app.get("/", function(req, res){
+   res.redirect("./public");
+});
+
+server.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
+
+io.on('connection', function(socket){
+    
+    }
+);
---
+++ public/script.js
@@ -0,0 +1,12 @@
+/*var socket = io.connect('http://localhost:3000');
+
+var This_Player = prompt("Enter your nickname");
+socket.emit("AddPlayer", This_Player);*/
+
+function setup() {
+    var canvas = createCanvas(768, 768);
+    console.log("done");
+    canvas.parent('Canvas_Holder');
+    background(111,111,111);
+
+}
